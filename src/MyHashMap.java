d5019095295122bf6fe5b5901ae12d80b40cd606
Jan <jan.rehnert@fau.de>
2023-04-17 13:14:09 +0200

    Added some comments and the feature of a collision-linked-list

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -8,25 +8,44 @@ public class MyHashMap<K, V> {
     private int size = 0;
     private int capacity = 500;
 
+
+    /**
+     * Entry class, with a LinkedList feature when collision happens
+     * @param <K> Key of the entry
+     * @param <V> Value of the entry
+     */
     private static class Entry<K, V> {
         K key;
         V value;
+        Entry<K, V> next;
 
-        public Entry(K key, V value) {
+        public Entry(K key, V value, Entry<K, V> next) {
             this.key = key;
             this.value = value;
+            this.next = next;
         }
     }
 
+    /**
+     * Default constructor
+     */
     public MyHashMap() {
         entries = new Entry[capacity];
     }
 
+    /**
+     * Constructor with given capacity
+     * @param capacity How much buckets are created
+     */
     public MyHashMap(int capacity) {
         this.capacity = capacity;
         entries = new Entry[capacity];
     }
 
+    /**
+     * Constructor which clones the given MyHashMap
+     * @param map That should be cloned
+     */
     public MyHashMap(MyHashMap<K, V> map) {
         this.entries = map.entries;
         this.capacity = map.capacity;
@@ -48,18 +67,29 @@ public class MyHashMap<K, V> {
         return this;
     }
 
+    /**
+     *
+     * @param key That should be searched for
+     * @return Returns true, when the MyHashMap contains the given value, false otherwise
+     */
     public boolean containsKey(Object key) {
         for(Entry<K, V> entry : entries) {
-            if(entry == null) continue;
-            if(entry.key == key) return true;
+            Entry<K, V> current = entry;
+            while(current != null) {
+                if(current.key == key) return true;
+                current = current.next;
+            }
         }
         return false;
     }
 
     public boolean containsValue(Object value) {
         for(Entry<K, V> entry : entries) {
-            if(entry == null) continue;
-            if(entry.value == value) return true;
+            Entry<K, V> current = entry;
+            while(current != null) {
+                if(current.value == value) return true;
+                current = current.next;
+            }
         }
         return false;
     }
@@ -67,15 +97,23 @@ public class MyHashMap<K, V> {
     public Set<MyHashMap.Entry<K, V>> entrySet() {
         Set<MyHashMap.Entry<K, V>> set = new HashSet();
         for(Entry<K, V> entry : entries) {
-            if(entry != null)
-                set.add(entry);
+            Entry<K, V> current = entry;
+            while(current != null) {
+                set.add(current);
+                current = current.next;
+            }
         }
         return set;
     }
 
     public V get(Object key) {
         int hash = hash(key);
-        return entries[hash].value;
+        Entry<K, V> current = entries[hash];
+        while(current != null) {
+            if(current.key == key) return current.value;
+            current = current.next;
+        }
+        return null;
     }
 
     public V getOrDeffault(Object key, V defaultValue) {
@@ -101,7 +139,7 @@ public class MyHashMap<K, V> {
         if(entries[hash] != null) {
             entries[hash].value = value;
         } else {
-            entries[hash] = new Entry<>(key, value);
+            entries[hash] = new Entry<>(key, value, null);
             size++;
         }
         return entries[hash].value;
@@ -122,6 +160,7 @@ public class MyHashMap<K, V> {
     }
 
     public void remove(Object key) {
+        int hash = hash(key);
 
     }
 
